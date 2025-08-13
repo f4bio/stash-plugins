21c550317bb688dd13d99866d5f8ee28d3c95a36
Fabio Tea <iam@f4b.io>
2025-08-13 23:28:48 +0200

    Enhance `organiz3rOnUpdate` plugin: add tag-based scene organization, refactor file handling, and improve processing logic

--- plugins/organiz3rOnUpdate/organiz3rOnUpdate.py
+++ plugins/organiz3rOnUpdate/organiz3rOnUpdate.py
@@ -10,7 +10,10 @@ per_page = 100
 skip_tag = "[organiz3rOnUpdate: Skip]"
 
 # Defaults if nothing has changed in the stash ui
-settings = {"useStudioHierarchy": True}
+settings = {
+    "useStudioHierarchy": True,
+    "useTags": "Amateur|/data/xxx/Amateur,Gangbang|/data/xxx/Gangbang"
+}
 
 json_input = json.loads(sys.stdin.read())
 
@@ -22,28 +25,68 @@ if "organiz3rOnUpdate" in config:
 log.info("config: {} ".format(settings))
 
 
-def make_filename(filename: str):
+def calculate_filename(filename: str):
     _filename = filename.lower()
 
     return _filename
 
 
-def make_directory(directory: str):
+def calculate_directory_name(directory: str):
     _directory = directory.lower()
 
     return _directory
 
 
+def move_scene(stash_scene: SceneInformation, destination_directory: str, destination_filename: str):
+    log.debug("move_scene(stash_scene={}, destination_directory={}, destination_filename={})".format(stash_scene,
+                                                                                                     destination_directory,
+                                                                                                     destination_filename))
+    log.debug("_new_filename: {}".format(destination_filename))
+    log.debug("_new_directory: {}".format(destination_directory))
+
+    _result = stash.move_files(dict({
+        "ids": [
+            stash_scene.id
+        ],
+        "destination_folder": destination_directory,
+        "destination_basename": destination_filename
+    }))
+    log.debug("move_files result: {}".format(_result))
+
+    return None
+
+
 def process_single_scene(stash_scene: SceneInformation):
-    log.debug("processing single scene: id={}".format(stash_scene.id))
+    log.debug("processing single scene: {}".format(stash_scene))
 
     # if the scene has [organiz3rOnUpdate: Skip] then skip it
     if skip_tag in stash_scene.tags:
-        log.debug("skipping scene")
+        log.info("skipping scene")
         return None
 
+    _new_filename = calculate_filename(stash_scene.filename)
+    _new_directory = "/tmp"
+
     if settings["useStudioHierarchy"]:
-        log.debug("useStudioHierarchy(stash_scene):" + json.dumps(stash_scene, indent=4))
+        log.debug("useStudioHierarchy(stash_scene)")
+
+        _studio = stash.find_studio(stash_scene.studio)
+        log.debug("found studio: {}".format(_studio))
+
+        _new_directory = calculate_directory_name(stash_scene.studio)
+
+    if settings["useTags"]:
+        log.debug("useTags(stash_scene)")
+
+        _tagsFolders = settings["useTags"].split(",")
+
+        for _tagFolder in _tagsFolders:
+            _tag, _folder = _tagFolder.split("|")
+            if _tag in stash_scene.tags:
+                _new_directory = calculate_directory_name(_folder)
+                break
+
+    move_scene(stash_scene, _new_directory, _new_filename)
 
     return None
 
@@ -51,31 +94,13 @@ def process_single_scene(stash_scene: SceneInformation):
 def process_all_scenes():
     log.debug("processing all scenes")
     all_scenes = stash.find_scenes({}, {}, get_count=True)
-    log.info("{} scenes to process.".format(len(all_scenes)))
+    log.info("{} scenes to process.".format(all_scenes[0]))
 
     for scene in all_scenes[1]:
         log.debug("processing scene: {}".format(scene))
         scene_information = SceneInformation(scene)
 
-        log.debug("extracted scene information: {}".format(json.dumps(scene_information)))
-
-        _studio = stash.find_studio(scene_information.studio)
-        log.debug("found studio: {}".format(_studio))
-
-        _new_filename = make_filename(scene_information.filename)
-        _new_directory = make_directory(scene_information.directory)
-
-        log.debug("_new_filename: {}".format(_new_filename))
-        log.debug("_new_directory: {}".format(_new_directory))
-
-        _result = stash.move_files(dict({
-            "ids": [
-                scene_information.id
-            ],
-            "destination_folder": scene_information.studio,
-            "destination_basename": _new_filename
-        }))
-        log.debug("move result: {}".format(_result))
+        process_single_scene(scene_information)
 
     return None
 
